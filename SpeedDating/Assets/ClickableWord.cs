6d36031a0f0aad107204a481afa368da1b74ebc1
William Pheloung <William Pheloung>
2016-02-01 13:22:30 -0500

    word color interpolation

--- SpeedDating/Assets/ClickableWord.cs
+++ SpeedDating/Assets/ClickableWord.cs
@@ -9,6 +9,8 @@ public class ClickableWord : MonoBehaviour {
 		gameObject.AddComponent<BoxCollider2D> ();
 		t.text = w._text;
 		word = w;
+		StartCoroutine (InterpolateRandomly ());
+		t.color = Random.ColorHSV ();
 	}
 
 	public void OnClick(){
@@ -19,6 +21,26 @@ public class ClickableWord : MonoBehaviour {
 	public void OnMouseDown()
 	{
 		OnClick ();
+	}
+
+	public IEnumerator InterpolateRandomly(){
+		while (true) {
+			//pick a random color
+			Color lastColor = t.color;
+			Color randomColor = Random.ColorHSV();
+			//interpolate to that color
+
+			float lerpTime = Random.Range (.2f, 1.0f);
+			float timePassed = 0.0f;
+			while (timePassed < lerpTime) {
+				timePassed += Time.deltaTime;
+				t.color = Color.Lerp (lastColor, randomColor, timePassed / lerpTime);
+
+				yield return new WaitForEndOfFrame ();
+			}
+
+		}
+
 	}
 
 }
